12c8eef5c723cdc5971c42d67f1119ed12c53857
Kuan-Lin Chen <72702965+tzamou@users.noreply.github.com>
2023-06-06 21:23:48 +0800

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,17 @@
+function showpub() {
+			var x = document.getElementById("hiddenpub");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+			} else {
+				x.style.display = "none";
+			}
+		}
+
+function showpypi() {
+			var x = document.getElementById("hiddenpypi");
+			if (x.style.display === "none") {
+				x.style.display = "block";
+			} else {
+				x.style.display = "none";
+			}
+		}
